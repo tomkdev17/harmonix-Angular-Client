3f26c52aa95b4b4a961e16345ad5d16a5f15ddb2
tomkdev17 <143559210+tomkdev17@users.noreply.github.com>
2024-10-29 16:27:46 -0500

    Include Angular Material, create user-registration-component

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+  
+  @Input() userData = {Username: '', Password: '', Email: '', Birthday: ''};
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {}
+
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      //successful user registration to go here later
+
+      this.dialogRef.close();
+      
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      }); 
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    })
+  }
+}
